207fbc3a476dc1bb39ef5cf0768f3c01afba52b0
selin <selina.kuikanmaki@metropolia.fi>
2026-02-11 12:51:19 +0200

    vko2

--- Viikko 2 mod03/3.4.py
+++ Viikko 2 mod03/3.4.py
@@ -2,7 +2,7 @@
 vuosiluku = int(input("Vuosiluku: "))
 if vuosiluku %4 == 0 and vuosiluku < 100:
     print("Vuosiluku on karkausvuosi")
-elif vuosiluku %100 == 0 and vuosiluku > 100 and vuosiluku %400 == 0:
+elif vuosiluku %100 == 0 and vuosiluku %400 == 0:
         print("Vuosiluku on karkausvuosi")
 else:
     print("Vuosiluku ei ole karkausvuosi")
